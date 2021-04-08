706c96335a7c956b7c6b2ef79ebfaf08d45e567f
astrimbu <1097487+astrimbu@users.noreply.github.com>
2021-04-08 17:53:26 -0500

    Filter/hide completed tasks, task count appended to title

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,44 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { TaskForm } from './TaskForm';
 
 const toggleChecked = ({ _id, isChecked }) => {
-	TasksCollection.update (_id, {
-		$set: {
-			isChecked: !isChecked
-		}
-	})
+    TasksCollection.update (_id, {
+        $set: {
+            isChecked: !isChecked
+        }
+    });
 };
 
 const deleteTask = ({ _id }) => TasksCollection.remove (_id);
 
 export const App = () => {
-	const tasks = useTracker (() => TasksCollection.find ({}, { sort: { createdAt: -1 } }).fetch ());
+    const [hideCompleted, setHideCompleted] = useState (false);
+    const hideCompletedFilter = { isChecked: { $ne: true } };
 
-	return (
-		<div className="app">
-			<header>
-				<div className="app-bar">
-					<div className="app-heder">
-						<h1>📝️ To Do List</h1>
-					</div>
-				</div>
-			</header>
-			
-			<div className="main">
-				<TaskForm/>
 
-				<ul className="tasks">
-					{ tasks.map (task => <Task
-							key={ task._id }
-							task={ task }
-							onCheckboxClick={ toggleChecked }
-							onDeleteClick={ deleteTask }
-					/>) }
-				</ul>
-			</div>
-		</div>
-	);
+    const tasks = useTracker (() =>
+        TasksCollection.find (hideCompleted ? hideCompletedFilter : {} , {
+            sort: { createdAt: -1 }
+        }).fetch ()
+    );
+
+    const pendingTasksCount = useTracker (() =>
+        TasksCollection.find (hideCompletedFilter).count ()
+    );
+
+    const pendingTasksTitle = `${
+        pendingTasksCount ? ` (${pendingTasksCount})` : ''
+    }`;
+
+    return (
+        <div className="app">
+            <header>
+                <div className="app-bar">
+                    <div className="app-heder">
+                        <h1>
+                            📝️ To Do List
+                            {pendingTasksTitle}
+                        </h1>
+                    </div>
+                </div>
+            </header>
+
+            <div className="main">
+                <TaskForm/>
+                <div className="filter">
+                    <button onClick={() => setHideCompleted (!hideCompleted)}>
+                        {hideCompleted ? 'Show All' : 'Hide Completed'}
+                    </button>
+                </div>
+
+                <ul className="tasks">
+                    { tasks.map (task => <Task
+                        key={ task._id }
+                        task={ task }
+                        onCheckboxClick={ toggleChecked }
+                        onDeleteClick={ deleteTask }
+                    />) }
+                </ul>
+            </div>
+        </div>
+    );
 };
