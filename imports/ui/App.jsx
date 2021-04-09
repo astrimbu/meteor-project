ed26e6698bc3f70648b6cf7f5f2849ec89e83e8d
astrimbu <1097487+astrimbu@users.noreply.github.com>
2021-04-09 15:01:32 -0500

    Just testing a git branch thing v2

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,8 @@ import { TasksCollection } from '/imports/db/TasksCollection';
 import { TaskForm } from './TaskForm';
 import { useTracker } from 'meteor/react-meteor-data';
 
+// test-branch 2, return of the test
+
 const toggleChecked = ({ _id, isChecked }) => {
     Meteor.call ('tasks.setIsChecked', _id, !isChecked);
 };
